38a3ecd55ab18732d9f3b335004e0d3125f5ff55
henimezrani <henimezrani@gmail.com>
2020-01-04 09:19:09 +0100

    Somehow complete

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -11,21 +11,3 @@ db.once('open', function() {
   console.log('mongoose connected successfully');
 });
 
-var itemSchema = mongoose.Schema({
-  quantity: Number,
-  description: String
-});
-
-var Item = mongoose.model('Item', itemSchema);
-
-var selectAll = function(callback) {
-  Item.find({}, function(err, items) {
-    if(err) {
-      callback(err, null);
-    } else {
-      callback(null, items);
-    }
-  });
-};
-
-module.exports.selectAll = selectAll;
